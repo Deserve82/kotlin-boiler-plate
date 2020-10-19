4986ff46d7c434ef0dd4a595a32c049b8d4da2c1
deserve82 <inth8204@gmail.com>
2020-10-20 00:02:49 +0900

    add review list

--- FaMin copy/app/src/main/java/com/example/famin/fragment/ReviewFragment.kt
+++ FaMin copy/app/src/main/java/com/example/famin/fragment/ReviewFragment.kt
@@ -8,8 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import com.example.famin.R
+import com.example.famin.fragment.MarketInfo.ReviewListAdaptor
 import com.example.famin.fragment.MarketInfo.WriteActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.fragment_review.view.*
 
 // TODO: Rename parameter arguments, choose names that match
@@ -26,6 +28,12 @@ class ReviewFragment : Fragment() {
 
     private lateinit var auth : FirebaseAuth
 
+    private val db = FirebaseFirestore.getInstance()
+
+    private val text_array = ArrayList<String>()
+    private val nickname_array = ArrayList<String>()
+    private val rating_array = ArrayList<String>()
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -34,6 +42,20 @@ class ReviewFragment : Fragment() {
 
         auth = FirebaseAuth.getInstance()
 
+        val review_adapter = ReviewListAdaptor(requireContext(), nickname_array, text_array, rating_array)
+        view.listview_review.adapter = review_adapter
+
+        db.collection("reviews")
+            .get()
+            .addOnSuccessListener { result ->
+                for (document in result){
+                    rating_array.add(document.get("rating") as String)
+                    text_array.add(document.get("text") as String)
+                    nickname_array.add(document.get("writer") as String)
+                }
+                review_adapter.notifyDataSetChanged()
+            }
+            .addOnFailureListener { exception ->  }
         view.write_button.setOnClickListener{
             if (auth.currentUser == null){
                 Toast.makeText(requireContext(), "가입 또는 로그인을 해주세요", Toast.LENGTH_LONG).show()
